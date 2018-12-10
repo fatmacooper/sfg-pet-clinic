e25297535c228fcc7570d2d4798335fbc5121564
fatma arik copur <fatmaarik@gmail.com>
2018-12-10 01:36:52 -0500

    auto generation of ID is added. closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgclient/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgclient/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package guru.springframework.sfgclient.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgclient.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<> (map.values());
@@ -16,8 +15,12 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    T save(T object){
+        if (object != null) {
+            map.put(this.getNextId(), object);
+        }else{
+            throw new RuntimeException("Object not found!!!");
+        }
         return object;
     }
 
@@ -27,5 +30,15 @@ public abstract class AbstractMapService<T,ID> {
 
     void delete(T object){
         map.entrySet().removeIf(entry ->entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try{
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch(NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
