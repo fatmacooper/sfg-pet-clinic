607e062bc1aceb36bfceb4df576c0e0dc4c17fd4
fatma arik copur <fatmaarik@gmail.com>
2018-12-18 00:53:33 -0500

    added project lombok. closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgclient/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgclient/model/Pet.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgclient.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="pets")
 public class Pet extends BaseEntity{
@@ -18,43 +25,6 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name="owner_id")
     private Owner owner;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
-    public String getName() {return name;}
-
-    public void setName(String name) {this.name = name; }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
